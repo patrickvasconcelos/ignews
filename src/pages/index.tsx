648626775a75fb1598f9ef70a665e39868da17dd
Patrick Vasconcelos <patrickvs@hotmail.com>
2021-07-04 02:28:03 -0300

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import Head from 'next/Head'
+import styles from './home.module.scss'
+
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about<br />the <span>React</span> world</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.99 month</span>
+          </p>
+        </section>
+        <img src='/images/avatar.svg' alt='Girl Codding' />
+      </main>
     </>
   )
 }
