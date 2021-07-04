2abe282b2bc1b9e6d4c61f6026a7eb297e51f65c
Patrick Vasconcelos <patrickvs@hotmail.com>
2021-07-04 01:23:41 -0300

    initial settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/Head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>Hello world</h1>
+    </>
+  )
+}
