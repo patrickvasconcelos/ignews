3e6dbb909601f46165860144e3716158d2cfbabb
Patrick Vasconcelos <patrickvs@hotmail.com>
2021-07-05 17:40:59 -0300

    call to sripe API

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,13 @@
 import styles from './styles.module.scss'
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
 
+interface SubscribeButtonProps {
+    priceId: string;
+}
 
-export function SubscribeButton() {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     return (
         <button type='button' className={styles.subscribeButton}>
             Subscribe
         </button>
-    ) 
+    )
 }
